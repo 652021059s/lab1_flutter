f1e24c8f8de2d8a5d47fca2c2131720632f291fa
652021059s <papangkornsaeoueng5@gmail.com>
2024-08-31 12:03:59 +0700

    first commit

--- lib/controllers/auth_service.dart
+++ lib/controllers/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:lab1_flutter/models/user_models.dart';
+import 'package:lab1_flutter/screens/register.dart';
 import 'package:lab1_flutter/screens/varibies.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
@@ -13,6 +14,24 @@ body: jsonEncode(
   {
   "username":username,
   "password":password,
+}
+));
+print(response.statusCode);
+  }
+  
+
+
+   Future <void>Register(String username, String password,String name,String role ) async{
+    print(apiURL);
+
+final response = await http.post(Uri.parse("$apiURL/api/user/register"),
+headers:{"Content-Type":"application/json"},
+body: jsonEncode(
+  {
+  "username":username,
+  "password":password,
+  "name":name,
+  "role":role
 }
 ));
 print(response.statusCode);
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab1_flutter/controllers/auth_service.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -6,6 +7,7 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>(); // ประกาศ _formKey
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
@@ -15,6 +17,29 @@ class _RegisterPageState extends State<RegisterPage> {
   String _selectedRole = '';
 
   final List<String> _roles = ['User', 'Admin']; // รายการตัวเลือก
+
+  void _register() async {
+    if (_formKey.currentState!.validate()) {
+      print('Username: ${_usernameController.text}');
+      print('Password: ${_passwordController.text}');
+      print('Name: ${_nameController.text}');
+      print('Role: $_selectedRole');
+
+      try {
+        // สมมติว่า `auth().login` เป็นฟังก์ชันที่มีอยู่และใช้ได้
+        final user = await AuthService().Register(
+          _usernameController.text,
+          _passwordController.text,
+          _nameController.text,
+          _selectedRole,
+        );
+ 
+        // เพิ่มการจัดการหลังจากการลงทะเบียนสำเร็จที่นี่ (เช่น เปลี่ยนหน้า)
+      } catch (e) {
+        
+      }
+    }
+  }
 
   void _togglePasswordVisibility() {
     setState(() {
@@ -30,71 +55,90 @@ class _RegisterPageState extends State<RegisterPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                prefixIcon: Icon(Icons.person), // ไอคอนสำหรับ Username
+        child: Form( // ใช้ Form widget และกำหนด key
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  prefixIcon: Icon(Icons.person), // ไอคอนสำหรับ Username
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a username';
+                  }
+                  return null;
+                },
               ),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.lock), // ไอคอนสำหรับ Password
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility,
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock), // ไอคอนสำหรับ Password
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: _togglePasswordVisibility,
                   ),
-                  onPressed: _togglePasswordVisibility,
+                ),
+                obscureText: _obscureText,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Name',
+                  prefixIcon: Icon(Icons.person_outline), // ไอคอนสำหรับ Name
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              // แสดง ChoiceChips
+              Wrap(
+                spacing: 10.0,
+                children: _roles.map((role) {
+                  return ChoiceChip(
+                    label: Text(role),
+                    selected: _selectedRole == role,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedRole = selected ? role : '';
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _register, // เรียกใช้งานฟังก์ชันลงทะเบียน
+                child: Text('Register'),
+              ),
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Already have an account? Login',
+                  style: TextStyle(color: Colors.blue),
                 ),
               ),
-              obscureText: _obscureText,
-            ),
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Name',
-                prefixIcon: Icon(Icons.person_outline), // ไอคอนสำหรับ Name
-              ),
-            ),
-            SizedBox(height: 20),
-            // แสดง ChoiceChips
-            Wrap(
-              spacing: 10.0,
-              children: _roles.map((role) {
-                return ChoiceChip(
-                  label: Text(role),
-                  selected: _selectedRole == role,
-                  onSelected: (bool selected) {
-                    setState(() {
-                      _selectedRole = selected ? role : '';
-                    });
-                  },
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Handle register logic here
-              },
-              child: Text('Register'),
-            ),
-            SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Text(
-                'Already have an account? Login',
-                style: TextStyle(color: Colors.blue),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
